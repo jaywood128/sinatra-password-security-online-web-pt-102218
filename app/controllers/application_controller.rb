2e322cac0b5598ac05df73cf3cd6e473215a11b0
Johnathon Wood <jaywood128@gmail.com>
2019-02-18 17:39:56 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,6 +29,14 @@ end
 	get "/login" do
 		erb :login
 	end
+	post "/login" do
+  user = User.find_by(:username => params[:username])
+  if user
+    redirect "/success"
+  else
+    redirect "/failure"
+  end
+end
 
 	get "/success" do
 		if logged_in?
